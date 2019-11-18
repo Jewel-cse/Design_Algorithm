4c17384ce0e85ed4212bf80a645f83ff0387f44c
Md Jewel <md.jewel.cse@gmail.com>
2019-11-18 20:52:44 +0600

    add a new folder Number_Theory

---
+++ Number_theory/Modular_arithmetic.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m,x,i,cnt=0;
+  do{
+    cout << "enter the number :"<<endl;
+    cin >> n ;
+    cout << "enter the modiulo:" << endl;
+    cin >> m ;
+
+    x = n;
+    //if n is negative
+    if(n<0){
+        i = 1;
+        do{
+            x = n+(i*m);
+            i++;
+        }while(x<1);
+
+    }
+    //cout << x << endl;
+    x = x%m;
+    cout << "The modiulo is : "<<x;
+    cout <<"\n\n";
+  }while(++cnt<5); // for 5 test ;
+}
